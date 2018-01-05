918a182933717dbbbaa62bf3443d48b09cd91475
kate-tanzhou <2315611897@qq.com>
2018-01-05 15:58:20 +0800

    修改

--- src/dao/B.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public class B {
-	public static void main(String[] args) {
-		System.out.println("这是组长的feelstlye");
-	}
-}
--- src/entity/test.java
+++
@@ -1,8 +0,0 @@
-package entity;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println("´¢¶ş¹·ÄãºÃ");
-	}
-
-}
